b45cf2251d6f3fc50d350bf7dcfe2d14c7e71ac5
Adam Perez <amperez0208@gmail.com>
2023-01-07 09:42:01 -0500

    able to create post

--- src/components/createPost/CreatePost.jsx
+++ src/components/createPost/CreatePost.jsx
@@ -1,120 +1,125 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./createPost.css"
 import { useNavigate } from 'react-router-dom';
 import { url } from '../../App'
 
 const CreatePost = ({
     user,
-    setTitle,
-    setDescription,
-    setPrice,
-    setWillDeliver,
-    title,
-    description,
-    price,
-    willDeliver,
     error,
     token,
-    location,
-    setLocation,
     fetchPosts,
     setError,
   }) => {
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [price, setPrice] = useState("")
+    const [location, setLocation] = useState("")
+    const [willDeliver, setWillDeliver] = useState("")
+
     const navigate = useNavigate();
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch(`${url}/posts`, {
+        
+
+        try {
+      const response = await fetch(`${url}/posts`, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
-            post: {
+          post: {
             title,
             description,
             price,
             location,
             willDeliver,
-        },
-      }),
-    });
-        const data = await response.json();
-        if (data.error) {
+          },
+        }),
+      });
+      const data = await response.json();
+      if (data.error) {
         return setError(data.error.message);
+      }
+      fetchPosts();
+      setTitle("");
+      setDescription("");
+      setPrice("");
+      setLocation("");
+      navigate("/posts");
+    } catch (error) {
+      console.error(error);
+      setError("Something went wrong. Please try again later.");
     }
-    fetchPosts();
-    setTitle("");
-    setDescription("");
-    setPrice("");
-    setLocation("");
-    navigate("/posts");
+};
+    
+    
+        return (
+            <div className="create_post_container">
+            <h1>Create a New Post</h1>
+            <form className="new_post" onSubmit={handleSubmit}>
+            <input
+                className="new_posts"
+                type="text"
+                required
+                value={title}
+                placeholder="Title"
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+              />
+              <input
+                className="new_posts"
+                type="text"
+                required
+                value={description}
+                placeholder="Description"
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }}
+              />
+              <input
+                className="new_posts"
+                type="text"
+                required
+                value={price}
+                placeholder="Price"
+                onChange={(e) => {
+                  setPrice(e.target.value);
+                }}
+              />
+              <input
+                className="new_posts"
+                type="text"
+                value={location}
+                placeholder="Location"
+                onChange={(e) => {
+                  setLocation(e.target.value);
+                }}
+              />
+              <div className='select_delivery'>
+              <h3>Delivery?</h3>
+              <select
+                
+                name="Delivery?"
+                value={willDeliver}
+                onChange={(e) => {
+                    setWillDeliver(e.target.value);
+                  }}
+                >
+                  <option value="true">Yes</option>
+                  <option value="false">No</option>
+                </select>
+                </div>
+                <button type="submit">Submit Post</button>
+              </form>
+              <p>{error}</p>
+              </div>
+          );
+      
   };
   
-    if(user){
-        return (
-        <div className="create_post_container">
-        <h1>Create a New Post</h1>
-        <form className="new_post" onSubmit={handleSubmit}>
-        <input
-            className="new_posts"
-            type="text"
-            required
-            value={title}
-            placeholder="Title"
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-          <input
-            className="new_posts"
-            type="text"
-            required
-            value={description}
-            placeholder="Description"
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
-          />
-          <input
-            className="new_posts"
-            type="text"
-            required
-            value={price}
-            placeholder="Price"
-            onChange={(e) => {
-              setPrice(e.target.value);
-            }}
-          />
-          <input
-            className="new_posts"
-            type="text"
-            value={location}
-            placeholder="Location"
-            onChange={(e) => {
-              setLocation(e.target.value);
-            }}
-          />
-          <label>Delivery?</label>
-          <select
-            id="select_delivery"
-            name="Delivery?"
-            value={willDeliver}
-            onChange={(e) => {
-              setWillDeliver(e.target.value);
-            }}
-          >
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-          </select>
-          <button type="submit">Submit Post</button>
-        </form>
-        <p>{error}</p>
-        </div>
-  );
-} else {
-    return null;
-}
-};
-
-export default CreatePost
+  export default CreatePost
+  
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -23,6 +23,7 @@ const Navbar = ({setUser, user}) => {
           <li><Link to="/posts">Posts</Link></li>
           {user && localStorage.getItem("token") ? (
               <>
+              <li><Link to="/createpost">Sell</Link></li>
               <li><Link to="/profile">Profile</Link></li>
               <li><Link to="/" onClick={handleLogout}>Logout</Link></li>
               </>
